b64cb7d3cff5b90c0d6c5d2547586fd2da3e7cc3
Bruyn <bruyn777@gmail.com>
2021-05-03 10:37:02 +0300

    +outline and color door button

--- Rina_Diplom/Assets/Assets/Door/DoorOpen.cs
+++ Rina_Diplom/Assets/Assets/Door/DoorOpen.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorOpen : MonoBehaviour
@@ -10,12 +8,53 @@ public class DoorOpen : MonoBehaviour
 
     public int itemId;
 
+    public Material opened;
+    public Material closed;
+
+    [SerializeField]
+    private Color outlineColorOpened = Color.white;
+    
+    [SerializeField]
+    private Color outlineColorClosed = Color.white;
+    
+    private DTInventory.DTInventory _inventory;
+
+    private MeshRenderer _meshRenderer;
+
+    private Outline _outline;
+    
+    private void Start()
+    {
+        _outline = GetComponent<Outline>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+        
+        if (_inventory == null)
+            _inventory = FindObjectOfType<DTInventory.DTInventory>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (_inventory.HasItem(itemId))
+        {
+            _meshRenderer.material = opened;
+            SetOutlineColor(outlineColorOpened);
+        }
+        else
+        {
+            _meshRenderer.material = closed;
+            SetOutlineColor(outlineColorClosed);
+        }
     }
 
+    void SetOutlineColor(Color value)
+    {
+        if (_outline != null)
+        {
+            _outline.OutlineColor = value;
+        }
+    }
+    
     public void Open()
     {
         anim.Play();
