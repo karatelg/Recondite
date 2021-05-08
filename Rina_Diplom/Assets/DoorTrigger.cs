d7b7a48b21f981ba2a0013ce0836cbb675976a5b
Bruyn <bruyn777@gmail.com>
2021-05-08 15:58:33 +0300

    +add door trigger

---
+++ Rina_Diplom/Assets/DoorTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public Animation anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.Play();
+        }
+    }
+}
